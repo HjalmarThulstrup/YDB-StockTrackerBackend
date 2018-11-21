aecea2617f3a07cc1e98f3f47d020aca18e0a238
RasmusLumholdt <dev.rlumholdt@gmail.com>
2018-11-21 16:31:10 +0100

    Added test db

---
+++ src/test/java/usertest/TestDBSetup.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package usertest;
+
+import entity.User;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author rlumh
+ */
+public class TestDBSetup {
+
+    EntityManager em;
+
+    public TestDBSetup() {
+        
+    }
+
+    @Before
+    public void setUp() {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpaputest");
+        em = emf.createEntityManager();
+    }
+
+    @Test
+    public void testAddUser() {
+        System.out.println("addUser: test");
+        User u = new User("test", "1234");
+        System.out.println(u.toString());
+        User result = null;
+
+        try {
+            em.getTransaction().begin();
+            em.persist(u);
+            em.getTransaction().commit();
+
+            result = em.find(User.class, "test");
+        } catch (Exception e) {
+            //Skal nok kastes en custom exception her
+        } finally {
+            em.close();
+        }
+
+        assertNotNull(result);
+        assertTrue(u.getUserName().equals("test"));
+
+    }
+}

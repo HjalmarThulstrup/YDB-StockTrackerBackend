0f6d1155b3f199e9bb3eac1a27ed4957b8628622
David-M-Nielsen <wick3dwick@gmail.com>
2018-11-25 13:49:14 +0100

    made endpoint for removing stock from user list. Refactored path in addstocktouserlist

--- src/main/java/rest/UserResource.java
+++ src/main/java/rest/UserResource.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.annotation.security.RolesAllowed;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -78,7 +79,7 @@ public class UserResource
      */
     @POST
     @Produces(MediaType.APPLICATION_JSON)
-    @Path("/{username}/{stock}")
+    @Path("/{username}/add/{stock}")
     @RolesAllowed("user")
     public Response addStockToList(@PathParam("username") String userName, @PathParam("stock") String symbol)
     {
@@ -93,6 +94,37 @@ public class UserResource
                 return Response.ok("Stock added").build();
             } else {
                 return Response.ok("Stock not added").build();
+            }
+        } else {
+            return Response.ok("Not allowed").build();
+        }
+    }
+
+    /**
+     * Takes in the users name and returns a list of said user's stocks if it is
+     * the user who is logged in.
+     *
+     * @param userName
+     * @param symbol
+     * @return a json array of stocks
+     */
+    @PUT
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/{username}/remove/{stock}")
+    @RolesAllowed("user")
+    public Response removeStockFromList(@PathParam("username") String userName, @PathParam("stock") String symbol)
+    {
+        String user = securityContext.getUserPrincipal().getName();
+
+        if (userName.equals(user)) {
+            StockMapper sm = StockMapper.getInstance("pu");
+
+            boolean result = sm.removeStockFromFavourites(new User(userName, ""), symbol);
+
+            if (result) {
+                return Response.ok("Stock removed").build();
+            } else {
+                return Response.ok("Stock not removed").build();
             }
         } else {
             return Response.ok("Not allowed").build();

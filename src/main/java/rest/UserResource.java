fd5f150cac8eaa6691f3c76f9c3da80492926557
David-M-Nielsen <wick3dwick@gmail.com>
2018-11-25 13:46:20 +0100

    made endpoint for adding stock to list

--- src/main/java/rest/UserResource.java
+++ src/main/java/rest/UserResource.java
@@ -1,9 +1,11 @@
 package rest;
 
 import entity.Stocks;
+import entity.User;
 import java.util.List;
 import javax.annotation.security.RolesAllowed;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -12,6 +14,7 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.SecurityContext;
 import javax.ws.rs.core.UriInfo;
+import mappers.StockMapper;
 import mappers.UserMapper;
 
 /**
@@ -47,7 +50,7 @@ public class UserResource
      */
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    @Path("{username}/list")
+    @Path("/{username}/list")
     @RolesAllowed("user")
     public Response getUserStockList(@PathParam("username") String userName)
     {
@@ -55,11 +58,42 @@ public class UserResource
 
         if (userName.equals(user)) {
             StockFetcher sf = new StockFetcher();
-            
+
             List<Stocks> userStockList = UserMapper.getInstance("pu").getUserStockList(userName);
-            
+
             Response result = sf.multiFetch(userStockList);
             return Response.ok(result).build();
+        } else {
+            return Response.ok("Not allowed").build();
+        }
+    }
+
+    /**
+     * Takes in the users name and returns a list of said user's stocks if it is
+     * the user who is logged in.
+     *
+     * @param userName
+     * @param symbol
+     * @return a json array of stocks
+     */
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/{username}/{stock}")
+    @RolesAllowed("user")
+    public Response addStockToList(@PathParam("username") String userName, @PathParam("stock") String symbol)
+    {
+        String user = securityContext.getUserPrincipal().getName();
+
+        if (userName.equals(user)) {
+            StockMapper sm = StockMapper.getInstance("pu");
+
+            boolean result = sm.addStockToFavourites(new User(userName, ""), symbol);
+
+            if (result) {
+                return Response.ok("Stock added").build();
+            } else {
+                return Response.ok("Stock not added").build();
+            }
         } else {
             return Response.ok("Not allowed").build();
         }
